9bc34c4ce5860e7a96fa51b7dff559fb2210e210
goodej <46459881+goodej@users.noreply.github.com>
2023-10-02 02:55:07 +0000

    add playground.py

---
+++ playground.py
@@ -0,0 +1,103 @@
+import os
+import json
+import plaid
+from plaid.model.products import Products
+from plaid.model.accounts_get_request import AccountsGetRequest
+from plaid.api import plaid_api
+from dotenv import load_dotenv
+from flask import jsonify
+
+load_dotenv()
+
+# Fill in your Plaid API keys - https://dashboard.plaid.com/account/keys
+PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
+PLAID_SECRET = os.getenv('PLAID_SECRET')
+# Use 'sandbox' to test with Plaid's Sandbox environment (username: user_good,
+# password: pass_good)
+# Use `development` to test with live users and credentials and `production`
+# to go live
+PLAID_ENV = os.getenv('PLAID_ENV', 'sandbox')
+# PLAID_PRODUCTS is a comma-separated list of products to use when initializing
+# Link. Note that this list must contain 'assets' in order for the app to be
+# able to create and retrieve asset reports.
+PLAID_PRODUCTS = os.getenv('PLAID_PRODUCTS', 'transactions').split(',')
+
+# PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
+# will be able to select institutions from.
+PLAID_COUNTRY_CODES = os.getenv('PLAID_COUNTRY_CODES', 'US').split(',')
+
+
+def empty_to_none(field):
+    value = os.getenv(field)
+    if value is None or len(value) == 0:
+        return None
+    return value
+
+host = plaid.Environment.Sandbox
+
+if PLAID_ENV == 'sandbox':
+    host = plaid.Environment.Sandbox
+
+if PLAID_ENV == 'development':
+    host = plaid.Environment.Development
+
+if PLAID_ENV == 'production':
+    host = plaid.Environment.Production
+
+# Parameters used for the OAuth redirect Link flow.
+#
+# Set PLAID_REDIRECT_URI to 'http://localhost:3000/'
+# The OAuth redirect flow requires an endpoint on the developer's website
+# that the bank website should redirect to. You will need to configure
+# this redirect URI for your client ID through the Plaid developer dashboard
+# at https://dashboard.plaid.com/team/api.
+PLAID_REDIRECT_URI = empty_to_none('PLAID_REDIRECT_URI')
+
+configuration = plaid.Configuration(
+    host=host,
+    api_key={
+        'clientId': PLAID_CLIENT_ID,
+        'secret': PLAID_SECRET,
+        'plaidVersion': '2020-09-14'
+    }
+)
+
+api_client = plaid.ApiClient(configuration)
+client = plaid_api.PlaidApi(api_client)
+
+products = []
+for product in PLAID_PRODUCTS:
+    products.append(Products(product))
+
+
+# We store the access_token in memory - in production, store it in a secure
+# persistent data store.
+access_token = os.get('SANDBOX_ITEM_TOKEN')
+# The payment_id is only relevant for the UK Payment Initiation product.
+# We store the payment_id in memory - in production, store it in a secure
+# persistent data store.
+payment_id = None
+# The transfer_id is only relevant for Transfer ACH product.
+# We store the transfer_id in memory - in production, store it in a secure
+# persistent data store.
+transfer_id = None
+
+item_id = os.get('SANDBOX_ITEM_ID')
+
+def info():
+    global access_token
+    global item_id
+    return jsonify({
+        'item_id': item_id,
+        'access_token': access_token,
+        'products': PLAID_PRODUCTS
+    })
+
+def pretty_print_response(response):
+  print(json.dumps(response, indent=2, sort_keys=True, default=str))
+
+
+# get accounts 
+request = AccountsGetRequest(access_token=access_token)
+response = client.accounts_get(request)
+pretty_print_response(response.to_dict())
